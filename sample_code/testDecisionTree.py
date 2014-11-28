03ccbcc6fcccc716a81a5898e3e5d05242918c4f
JimJarvis <jimfanspire@gmail.com>
2014-11-28 11:10:55 -0500

    Add matplotlib graph the learning curve

--- sample_code/testDecisionTree.py
+++ sample_code/testDecisionTree.py
@@ -1,5 +1,6 @@
 # test a classification algorithm
 from learning import *
+import matplotlib.pyplot as pt
 
 car_train = DataSet(name='../data/car/car_train', attrnames='buying maint doors persons lug_boot safety values', target='values')
 car_cv = DataSet(name='../data/car/car_cv', attrnames='buying maint doors persons lug_boot safety values', target='values')
@@ -12,7 +13,7 @@ cv_set = car_cv
 def run_test(learner, maxDeviation, 
 	learning_curve_interval, test_trials, random_shuffle,  
 	train_set, cv_set, test_set,
-	cv_scores=None):
+	cv_scores=None, graph=False):
 	learner.train(train_set)
 	if maxDeviation > 0 :
 		learner.prune(cv_set, maxDeviation)
@@ -24,9 +25,16 @@ def run_test(learner, maxDeviation,
 	print "Macro F1 score on test data: ", "%.5f" % test(learner, test_set)
 	res = learningcurve(learner, maxDeviation, train_set, cv_set, test_set, 
 		learning_curve_interval, test_trials, random_shuffle=random_shuffle)
-	print "train dataset size: ", [x[0] for x in res]
+
+	sizes = [x[0] for x in res]
+	f1vals = ["%.5f" % x[1][1] for x in res]
+	print "train dataset size: ", sizes
 	print "average node count on test dataset: ", ["%.2f" % x[1][0] for x in res]
-	print "average F1 score on test dataset: ", ["%.5f" % x[1][1] for x in res]
+	print "average F1 score on test dataset: ", f1vals
+	if graph:
+		pt.plot(sizes, f1vals)
+		pt.show()
+		
 
 learner = DecisionTreeLearner()
 interval = 100
@@ -34,7 +42,7 @@ interval = 100
 # test
 print "Before Pruning"
 print "========================="
-run_test(learner, 0, interval, 1, False, train_set, cv_set, test_set)
+run_test(learner, 0, interval, 1, False, train_set, cv_set, test_set, graph=True)
 
 # Pruning validation: select best maxDev
 maxdev = 0
@@ -54,5 +62,5 @@ print ''
 # test
 print "After Pruning"
 print "========================="
-run_test(learner, maxdev, interval, 1, False, train_set, cv_set, test_set)
+run_test(learner, maxdev, interval, 1, False, train_set, cv_set, test_set, graph=True)
 
